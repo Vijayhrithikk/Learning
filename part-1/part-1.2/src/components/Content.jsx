5f905f1091ae76fd6371d6a0e3756985870944d8
Vijayhrithikk <vijaymonish11@gmail.com>
2025-07-14 00:25:16 +0530

    1.2 done

---
+++ part-1/part-1.2/src/components/Content.jsx
@@ -0,0 +1,21 @@
+import Part from "./Part"
+const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+export default function Content(){
+    return(
+        <div>
+          <Part part1={part1}
+          exercises1={exercises1}/>
+          <Part part2={part2}
+          exercises2={exercises2}/>
+          <Part part3={part3}
+          exercises3={exercises3}/>
+
+        </div>
+    )
+}
