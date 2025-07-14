77d5812704eb883a89784ef4a8c26150844d38c7
Vijayhrithikk <vijaymonish11@gmail.com>
2025-07-14 18:55:13 +0530

    1.5 done

--- part-1/part-1.2/src/components/Content.jsx
+++ part-1/part-1.2/src/components/Content.jsx
@@ -1,26 +1,9 @@
-import Part from "./Part"
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-]
-
-export default function Content(){
-    return(
-        <div>
-          <Part parts={parts}/>
-          <Part parts={parts}/>
-          <Part parts={parts}/>
-
-        </div>
-    )
+export default function Content(props){
+  return(
+      <>
+        {props.parts.map((part, i) => (
+          <p key={i}>{part.name} {part.exercises}</p>
+        ))}
+      </>
+  )
 }
--- part-1/part-1.2/src/components/Header.jsx
+++ part-1/part-1.2/src/components/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header(props){
     return(
-        <h1>{props.course}</h1>
+        <h1>{props.course.name}</h1>
     )
 }
