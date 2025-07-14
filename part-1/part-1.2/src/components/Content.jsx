270ee069b7d97a01efa4cd1a73f26e281b9d6a54
Vijayhrithikk <vijaymonish11@gmail.com>
2025-07-14 18:40:36 +0530

    1.4 done

--- part-1/part-1.2/src/components/Content.jsx
+++ part-1/part-1.2/src/components/Content.jsx
@@ -1,26 +1,25 @@
 import Part from "./Part"
-const part1 = {
-  name: 'Fundamentals of React',
-  exercises: 10
-}
-const part2 = {
-  name: 'Using props to pass data',
-  exercises: 7
-}
-const part3 = {
-  name: 'State of a component',
-  exercises: 14
-}
+const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
 
 export default function Content(){
     return(
         <div>
-          <Part part1={part1}
-          exercises1={exercises1}/>
-          <Part part2={part2}
-          exercises2={exercises2}/>
-          <Part part3={part3}
-          exercises3={exercises3}/>
+          <Part parts={parts}/>
+          <Part parts={parts}/>
+          <Part parts={parts}/>
 
         </div>
     )
